e01ea62f5c838d3fb32acea60e6dd0cd557ae6cf
Paul Klemstine <paulklemstine@gmail.com>
2026-05-25 01:23:23 -0500

    Aether local tick 2026-05-25T06:23Z

--- Catalog/Applications/Packages/package_index.js
+++ Catalog/Applications/Packages/package_index.js
@@ -965,10 +965,10 @@ window.FUTURE_DIRECTIONS = [
       "Logic"
     ],
     "priority_score": 1.0,
-    "status": "available",
+    "status": "in_progress",
     "research_mode": "prove",
     "source_exp_id": "c1bdccd8",
-    "consumed_by_exp_id": "",
+    "consumed_by_exp_id": "9ac4e46d",
     "timestamp": "2026-05-25T00:54:40.932722+00:00"
   },
   {
@@ -1096,10 +1096,10 @@ window.FUTURE_DIRECTIONS = [
       "Logic"
     ],
     "priority_score": 1.0,
-    "status": "in_progress",
+    "status": "available",
     "research_mode": "prove",
     "source_exp_id": "8e448ab4",
-    "consumed_by_exp_id": "c4d51361",
+    "consumed_by_exp_id": "",
     "timestamp": "2026-05-25T01:35:36.634466+00:00"
   },
   {
