e66781daed70a13d679dc12555748b3676a9dd27
Paul Klemstine <paulklemstine@gmail.com>
2026-05-30 21:34:43 -0500

    Aether local tick 2026-05-31T02:34Z

--- Catalog/Applications/Packages/package_index.js
+++ Catalog/Applications/Packages/package_index.js
@@ -9,8 +9,8 @@ window.PACKAGE_INDEX = [
     "domain": "Algebra",
     "date": "2026-05-31T02:34:14Z",
     "exp_id": "f9d127dc",
-    "quality_score": null,
-    "quality": "unrated",
+    "quality_score": 0.7853926086956521,
+    "quality": "substantial",
     "pkg_num": 441
   },
   {
@@ -12798,8 +12798,8 @@ window.PACKAGE_GRAPH = {
       "shape": "tetrahedron",
       "date": "2026-05-31T02:34:14Z",
       "hue": 270,
-      "priority_score": null,
-      "quality": "unrated"
+      "priority_score": 0.7853926086956521,
+      "quality": "substantial"
     }
   ],
   "edges": [
