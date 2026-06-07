87979a79c2cf9bf59195907289c80dc155c9847c
Paul Klemstine <paulklemstine@gmail.com>
2026-06-07 05:17:38 -0500

    Aether local tick 2026-06-07T10:17Z

--- Catalog/Applications/Packages/package_index.js
+++ Catalog/Applications/Packages/package_index.js
@@ -9,8 +9,8 @@ window.PACKAGE_INDEX = [
     "exp_id": "21c68fe5",
     "filename": "infinite_games_against_death_immortality_strategie.json",
     "pkg_num": 403,
-    "quality": "unrated",
-    "quality_score": null,
+    "quality": "partial",
+    "quality_score": 0.3044,
     "title": "Mortality Games: Ordinal Survival Against Transfinite Adversaries"
   },
   {
@@ -12838,8 +12838,8 @@ window.PACKAGE_GRAPH = {
       "hue": 270,
       "id": "infinite_games_against_death_immortality_strategie",
       "primary_domain": "Bridges",
-      "priority_score": null,
-      "quality": "unrated",
+      "priority_score": 0.3044,
+      "quality": "partial",
       "shape": "icosahedron",
       "title": "Mortality Games: Ordinal Survival Against Transfinite Adversaries"
     }
