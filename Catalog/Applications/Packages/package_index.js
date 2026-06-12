1f7aaeede1f3e2444429fb8591391a30aa4c7920
Paul Klemstine <paulklemstine@gmail.com>
2026-06-12 02:40:43 -0500

    Aether local tick 2026-06-12T07:40Z

--- Catalog/Applications/Packages/package_index.js
+++ Catalog/Applications/Packages/package_index.js
@@ -9,8 +9,8 @@ window.PACKAGE_INDEX = [
     "exp_id": "3c9f09e3",
     "filename": "close_proofs_close_proofs_cobhams_theorem_1972_sta.json",
     "pkg_num": 14,
-    "quality": "unrated",
-    "quality_score": null,
+    "quality": "partial",
+    "quality_score": 0.6822099399599733,
     "title": "The Checkerboard Move: A Rank-One Markov Basis for the 2x2x2 No-Three-Way Interaction Model"
   },
   {
@@ -415,8 +415,8 @@ window.PACKAGE_GRAPH = {
       "hue": 270,
       "id": "close_proofs_close_proofs_cobhams_theorem_1972_sta",
       "primary_domain": "Algebra",
-      "priority_score": null,
-      "quality": "unrated",
+      "priority_score": 0.6822099399599733,
+      "quality": "partial",
       "shape": "tetrahedron",
       "title": "The Checkerboard Move: A Rank-One Markov Basis for the 2x2x2 No-Three-Way Interaction Model"
     }
