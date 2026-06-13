2df325b6e2e645ae00e07dd7ec2a5f3aad607526
Paul Klemstine <paulklemstine@gmail.com>
2026-06-13 09:06:28 -0500

    Aether local tick 2026-06-13T14:06Z

--- Catalog/Applications/Packages/package_index.js
+++ Catalog/Applications/Packages/package_index.js
@@ -9,8 +9,8 @@ window.PACKAGE_INDEX = [
     "exp_id": "81fcaebc",
     "filename": "the_foundational_file_catalogcomputationselfmodify.json",
     "pkg_num": 87,
-    "quality": "unrated",
-    "quality_score": null,
+    "quality": "partial",
+    "quality_score": 0.68776,
     "title": "Discrete Dynamics of Self-Modifying Computation"
   },
   {
@@ -2652,8 +2652,8 @@ window.PACKAGE_GRAPH = {
       "hue": 270,
       "id": "the_foundational_file_catalogcomputationselfmodify",
       "primary_domain": "Bridges",
-      "priority_score": null,
-      "quality": "unrated",
+      "priority_score": 0.68776,
+      "quality": "partial",
       "shape": "icosahedron",
       "title": "Discrete Dynamics of Self-Modifying Computation"
     }
