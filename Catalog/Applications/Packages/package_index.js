068d7df944512658b3afcd635a30fe045b2203ef
Paul Klemstine <paulklemstine@gmail.com>
2026-06-14 11:18:35 -0500

    Aether local tick 2026-06-14T16:18Z

--- Catalog/Applications/Packages/package_index.js
+++ Catalog/Applications/Packages/package_index.js
@@ -9,8 +9,8 @@ window.PACKAGE_INDEX = [
     "exp_id": "bc66959c",
     "filename": "applicationsboltzmannbridgeinterleavinggeodesiclea.json",
     "pkg_num": 160,
-    "quality": "unrated",
-    "quality_score": null,
+    "quality": "substantial",
+    "quality_score": 0.7748660869565218,
     "title": "The Interleaving Metric is Geodesic: Constant-Speed Paths Between Data Shapes"
   },
   {
@@ -4996,8 +4996,8 @@ window.PACKAGE_GRAPH = {
       "hue": 270,
       "id": "applicationsboltzmannbridgeinterleavinggeodesiclea",
       "primary_domain": "Bridges",
-      "priority_score": null,
-      "quality": "unrated",
+      "priority_score": 0.7748660869565218,
+      "quality": "substantial",
       "shape": "icosahedron",
       "title": "The Interleaving Metric is Geodesic: Constant-Speed Paths Between Data Shapes"
     }
