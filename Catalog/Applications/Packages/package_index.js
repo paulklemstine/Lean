d183609902a5a74c4740bd6514d8ac3f5ac56426
Paul Klemstine <paulklemstine@gmail.com>
2026-06-15 22:39:54 -0500

    Aether local tick 2026-06-16T03:39Z

--- Catalog/Applications/Packages/package_index.js
+++ Catalog/Applications/Packages/package_index.js
@@ -9,8 +9,8 @@ window.PACKAGE_INDEX = [
     "exp_id": "3fd7a316",
     "filename": "catalogbridgesfunctorialtropicalpythagoreanlean_0_.json",
     "pkg_num": 160,
-    "quality": "unrated",
-    "quality_score": null,
+    "quality": "substantial",
+    "quality_score": 0.8053600000000001,
     "title": "A Functorial Tropical Ultrametric on the Boundary of the Berggren Tree"
   },
   {
@@ -4946,8 +4946,8 @@ window.PACKAGE_GRAPH = {
       "hue": 90,
       "id": "catalogbridgesfunctorialtropicalpythagoreanlean_0_",
       "primary_domain": "Bridges",
-      "priority_score": null,
-      "quality": "unrated",
+      "priority_score": 0.8053600000000001,
+      "quality": "substantial",
       "shape": "icosahedron",
       "title": "A Functorial Tropical Ultrametric on the Boundary of the Berggren Tree"
     }
