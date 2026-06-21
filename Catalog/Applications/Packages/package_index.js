c4c460b1e7b6f1093ab2aadcd6422c8ac04e0c43
Paul Klemstine <paulklemstine@gmail.com>
2026-06-20 19:40:09 -0500

    Aether local tick 2026-06-21T00:40Z

--- Catalog/Applications/Packages/package_index.js
+++ Catalog/Applications/Packages/package_index.js
@@ -9,8 +9,8 @@ window.PACKAGE_INDEX = [
     "exp_id": "2683755a",
     "filename": "bounded_tree_independence_in_k_1d_free_graphs_excl.json",
     "pkg_num": 216,
-    "quality": "unrated",
-    "quality_score": null,
+    "quality": "partial",
+    "quality_score": 0.5860000000000001,
     "title": "Bounded Tree-Independence Number in K_{1,d}-Free Graphs Excluding a Planar Induced Minor"
   },
   {
@@ -6599,8 +6599,8 @@ window.PACKAGE_GRAPH = {
       "hue": 92,
       "id": "bounded_tree_independence_in_k_1d_free_graphs_excl",
       "primary_domain": "MachineLearning",
-      "priority_score": null,
-      "quality": "unrated",
+      "priority_score": 0.5860000000000001,
+      "quality": "partial",
       "shape": "sphere_rings",
       "title": "Bounded Tree-Independence Number in K_{1,d}-Free Graphs Excluding a Planar Induced Minor"
     }
