e273dbe32abc0cf42b471a29e0cf7009f5c02001
Paul Klemstine <paulklemstine@gmail.com>
2026-06-20 22:58:12 -0500

    Aether local tick 2026-06-21T03:58Z

--- Catalog/Applications/Packages/package_index.js
+++ Catalog/Applications/Packages/package_index.js
@@ -9,8 +9,8 @@ window.PACKAGE_INDEX = [
     "exp_id": "7a341625",
     "filename": "game_of_life_universality.json",
     "pkg_num": 219,
-    "quality": "unrated",
-    "quality_score": null,
+    "quality": "partial",
+    "quality_score": 0.5548463972823172,
     "title": "Union-Closed Families as Positive-Correlation Systems"
   },
   {
@@ -6686,8 +6686,8 @@ window.PACKAGE_GRAPH = {
       "hue": 272,
       "id": "game_of_life_universality",
       "primary_domain": "Bridges",
-      "priority_score": null,
-      "quality": "unrated",
+      "priority_score": 0.5548463972823172,
+      "quality": "partial",
       "shape": "icosahedron",
       "title": "Union-Closed Families as Positive-Correlation Systems"
     }
