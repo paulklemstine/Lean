bf13b18999171e22912782df2b6288975a44aa1b
Paul Klemstine <paulklemstine@gmail.com>
2026-06-24 10:18:55 -0500

    Aether local tick 2026-06-24T15:18Z

--- Catalog/Applications/Packages/package_index.js
+++ Catalog/Applications/Packages/package_index.js
@@ -9,8 +9,8 @@ window.PACKAGE_INDEX = [
     "exp_id": "72f1519b",
     "filename": "degree4_vertices_in_the_flip_graph_of_the_mn_miura.json",
     "pkg_num": 32,
-    "quality": "unrated",
-    "quality_score": null,
+    "quality": "partial",
+    "quality_score": 0.5317089325931801,
     "title": "Degree-4 Vertices in the Flip Graph of the Miura-ori: A Combinatorial Core"
   },
   {
@@ -937,8 +937,8 @@ window.PACKAGE_GRAPH = {
       "hue": 101,
       "id": "degree4_vertices_in_the_flip_graph_of_the_mn_miura",
       "primary_domain": "Pythagorean",
-      "priority_score": null,
-      "quality": "unrated",
+      "priority_score": 0.5317089325931801,
+      "quality": "partial",
       "shape": "triangular_prism",
       "title": "Degree-4 Vertices in the Flip Graph of the Miura-ori: A Combinatorial Core"
     }
