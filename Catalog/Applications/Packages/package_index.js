29ecdb5e0dcd2d117e60c34340edc62e6a98a2d6
Paul Klemstine <paulklemstine@gmail.com>
2026-06-26 13:05:51 -0500

    Aether local tick 2026-06-26T18:05Z

--- Catalog/Applications/Packages/package_index.js
+++ Catalog/Applications/Packages/package_index.js
@@ -9,8 +9,8 @@ window.PACKAGE_INDEX = [
     "exp_id": "3be846c2",
     "filename": "ramsey_theory_bounds_and_constructions.json",
     "pkg_num": 104,
-    "quality": "unrated",
-    "quality_score": null,
+    "quality": "partial",
+    "quality_score": 0.464016452173913,
     "title": "Ramsey Theory: Bounds and Constructions"
   },
   {
@@ -3034,8 +3034,8 @@ window.PACKAGE_GRAPH = {
       "hue": 280,
       "id": "ramsey_theory_bounds_and_constructions",
       "primary_domain": "Bridges",
-      "priority_score": null,
-      "quality": "unrated",
+      "priority_score": 0.464016452173913,
+      "quality": "partial",
       "shape": "icosahedron",
       "title": "Ramsey Theory: Bounds and Constructions"
     }
